2ea70cd3e19d0210766eb0a5f9ca599dd24613db
MrHowells <122268295+MrHowells@users.noreply.github.com>
2023-01-10 00:34:21 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,193 @@
+let Name = 0
+function girls () {
+    if (Name == 1) {
+        basic.showString("Amy")
+    }
+    if (Name == 2) {
+        basic.showString("April")
+    }
+    if (Name == 3) {
+        basic.showString("Arya")
+    }
+    if (Name == 4) {
+        basic.showString("Chloe B")
+    }
+    if (Name == 5) {
+        basic.showString("Chloe R")
+    }
+    if (Name == 6) {
+        basic.showString("Emily J")
+    }
+    if (Name == 7) {
+        basic.showString("Emily S")
+    }
+    if (Name == 8) {
+        basic.showString("Freya")
+    }
+    if (Name == 9) {
+        basic.showString("Grace")
+    }
+    if (Name == 10) {
+        basic.showString("Isla")
+    }
+    if (Name == 11) {
+        basic.showString("Lois")
+    }
+    if (Name == 12) {
+        basic.showString("Maisie")
+    }
+    if (Name == 13) {
+        basic.showString("Mia")
+    }
+    if (Name == 14) {
+        basic.showString("Tali")
+    }
+    if (Name == 15) {
+        basic.showString("Willow")
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    Name = randint(1, 15)
+    girls()
+})
+function whole_class () {
+    if (Name == 1) {
+        basic.showString("Amy")
+    }
+    if (Name == 2) {
+        basic.showString("April")
+    }
+    if (Name == 3) {
+        basic.showString("Arya")
+    }
+    if (Name == 4) {
+        basic.showString("Chloe B")
+    }
+    if (Name == 5) {
+        basic.showString("Chloe R")
+    }
+    if (Name == 6) {
+        basic.showString("Emily J")
+    }
+    if (Name == 7) {
+        basic.showString("Emily S")
+    }
+    if (Name == 8) {
+        basic.showString("Freya")
+    }
+    if (Name == 9) {
+        basic.showString("Grace")
+    }
+    if (Name == 10) {
+        basic.showString("Isla")
+    }
+    if (Name == 11) {
+        basic.showString("Lois")
+    }
+    if (Name == 12) {
+        basic.showString("Maisie")
+    }
+    if (Name == 13) {
+        basic.showString("Mia")
+    }
+    if (Name == 14) {
+        basic.showString("Tali")
+    }
+    if (Name == 15) {
+        basic.showString("Willow")
+    }
+    if (Name == 16) {
+        basic.showString("Casey")
+    }
+    if (Name == 17) {
+        basic.showString("Cole")
+    }
+    if (Name == 18) {
+        basic.showString("Freddie")
+    }
+    if (Name == 19) {
+        basic.showString("Jacob")
+    }
+    if (Name == 20) {
+        basic.showString("Kian")
+    }
+    if (Name == 21) {
+        basic.showString("Lucas")
+    }
+    if (Name == 22) {
+        basic.showString("Lycus")
+    }
+    if (Name == 23) {
+        basic.showString("Matthew")
+    }
+    if (Name == 24) {
+        basic.showString("Michael")
+    }
+    if (Name == 25) {
+        basic.showString("Teagan")
+    }
+    if (Name == 26) {
+        basic.showString("Thomas")
+    }
+    if (Name == 27) {
+        basic.showString("Tommy")
+    }
+    if (Name == 28) {
+        basic.showString("Treyell")
+    }
+    if (Name == 29) {
+        basic.showString("William")
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    Name = randint(16, 29)
+    boys()
+})
+input.onGesture(Gesture.Shake, function () {
+    Name = randint(1, 29)
+    whole_class()
+})
+function boys () {
+    if (Name == 16) {
+        basic.showString("Casey")
+    }
+    if (Name == 17) {
+        basic.showString("Cole")
+    }
+    if (Name == 18) {
+        basic.showString("Freddie")
+    }
+    if (Name == 19) {
+        basic.showString("Jacob")
+    }
+    if (Name == 20) {
+        basic.showString("Kian")
+    }
+    if (Name == 21) {
+        basic.showString("Lucas")
+    }
+    if (Name == 22) {
+        basic.showString("Lycus")
+    }
+    if (Name == 23) {
+        basic.showString("Matthew")
+    }
+    if (Name == 24) {
+        basic.showString("Michael")
+    }
+    if (Name == 25) {
+        basic.showString("Teagan")
+    }
+    if (Name == 26) {
+        basic.showString("Thomas")
+    }
+    if (Name == 27) {
+        basic.showString("Tommy")
+    }
+    if (Name == 28) {
+        basic.showString("Treyell")
+    }
+    if (Name == 29) {
+        basic.showString("William")
+    }
+}
